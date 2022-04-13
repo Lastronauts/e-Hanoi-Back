aa1b94d34b5cfcc1722568e6ff693fdabba11763
dino3616 <suraimu3616@outlook.jp>
2022-04-14 03:12:52 +0900

    feat: ✨ DBpool生成関数を実装.

---
+++ graphql/src/db/mod.rs
@@ -0,0 +1,43 @@
+use actix_web::web::Data;
+use anyhow::{
+    Context,
+    Result,
+};
+use diesel::{
+    r2d2::ConnectionManager,
+    PgConnection,
+};
+use r2d2::Pool;
+use std::env;
+
+mod schema;
+pub mod users;
+use users::loader::{
+    create_users_loader,
+    UsersLoader,
+};
+
+pub type PgPool = Pool<ConnectionManager<PgConnection>>;
+
+pub fn new_pool() -> Result<PgPool> {
+    let database_url = env::var("DATABASE_URL")?;
+
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+
+    Pool::builder()
+        .max_size(15)
+        .build(manager)
+        .context("failed to build database pool.")
+}
+
+pub struct Loaders {
+    pub users: UsersLoader,
+}
+
+impl Loaders {
+    pub fn new(pool: &Data<PgPool>) -> Self {
+        Self {
+            users: create_users_loader(pool),
+        }
+    }
+}
